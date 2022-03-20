533be2b5fd1278b991e71c52122d93f0f0d9f83e
has12zen <has12@iitbbs.ac.in>
2022-03-20 15:13:16 +0530

    Post getAll

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -1,9 +1,33 @@
 const Post = require("../models/postModel");
 const factory = require("./handlerFactory");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const APIFeatures = require("../utils/apiFeatures");
 
-exports.getAllPosts = factory.getAll(Post, {
-  path: "createdBy",
+exports.getAllPosts = catchAsync(async (req, res) => {
+  const features = new APIFeatures(
+    Post.find({
+      parentId: null,
+    }).populate({
+      path: "createdBy",
+    }),
+    req.query
+  )
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  // const doc = await features.query.explain();
+  const doc = await features.query;
+
+  res.status(200).json({
+    status: "success",
+    results: doc.length,
+    data: {
+      doc,
+    },
+  });
 });
 
 exports.getPost = factory.getOne(Post);
